eca0c81d3b0d2c661e9fdee89f5fc1bc6b7b84a5
joaomunizdev <jpedro747.jp@gmail.com>
2025-11-15 17:36:58 -0300

    feat: remove provider declaration

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,17 @@
 
 import PlansPage from "@/components/checkout/PlansPage";
 import ConfirmationPage from "@/components/checkout/ConfirmationPage";
-
-import { CheckoutProvider, useCheckout } from "@/context/CheckoutContext";
+import { useCheckout } from "@/context/CheckoutContext";
 import CheckoutPage from "@/components/checkout/CheckoutPage";
 
 function CheckoutFlowManager() {
   const { currentPage } = useCheckout();
 
-  if (currentPage === "checkout") {
-    return <CheckoutPage />;
-  }
-
-  if (currentPage === "confirmation") {
-    return <ConfirmationPage />;
-  }
-
+  if (currentPage === "checkout") return <CheckoutPage />;
+  if (currentPage === "confirmation") return <ConfirmationPage />;
   return <PlansPage />;
 }
 
 export default function HomePage() {
-  return (
-    <CheckoutProvider>
-      <CheckoutFlowManager />
-    </CheckoutProvider>
-  );
+  return <CheckoutFlowManager />;
 }
